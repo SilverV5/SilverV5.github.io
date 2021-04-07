e48f32e6df3253fa7a6c1fc533a89b5e2d5adcfc
SilverV4 <19217@gdc.school.nz>
2021-04-07 15:07:43 +1200

    First Website

---
+++ Script.js
@@ -0,0 +1,14 @@
+function placeOrder() {
+    "use strict";
+var orderForm = document.getElementById("my-form");
+results = "<h3>Success!</h3> Here is your order.";
+results += "<br>Name: " + orderForm.elements["my-name"].value;
+results += "<br>Address: " + orderForm.elements["my-address"].value;
+results += "<br>I like to order: " + orderForm.elements["my-monkey"].value;
+results += "<br>Quantity: " + orderForm.elements["my-qty"].value;
+var orderResults =document.getElementById("my-order");
+orderResults.style.display="block";
+orderResults.innerHTML = results;   
+}
+
+   
